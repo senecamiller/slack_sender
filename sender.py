5269ebb403e5b9b5249b07f348a86d0e037e5208
Seneca Miller <senecawyse@gmail.com>
2024-12-10 17:16:21 +0100

    Update sender.py
    removed the extra '' around the https:

--- sender.py
+++ sender.py
@@ -13,7 +13,7 @@ json_data = {
 }
 
 response = requests.post(
-    f''https://hooks.slack.com/services/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'/{os.environ.get("SLACK_ID")}',
+    f'https://hooks.slack.com/services/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/{os.environ.get("SLACK_ID")}',
     headers=headers,
     json=json_data,
 )
